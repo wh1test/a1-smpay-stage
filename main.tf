f48f43965660bced4990c41db30a607fe18888c8
besmart <rvv@besmart.by>
2020-05-02 20:29:39 +0300

    Removed PG servers

--- main.tf
+++ main.tf
@@ -52,157 +52,6 @@ resource "vcd_vapp_org_network" "infra-network" {
 #  storage_profile = "c01-cl02-Tier-2"
 #}
 
-###### vApp_Infra ######
-resource "vcd_vapp_vm" "smpay-t-gitlab-1" {
-  vapp_name     = "vApp_Infra"
-  name          = "smpay-t-gitlab-1"
-  computer_name = "smpay-t-gitlab-1"
-  catalog_name  = var.template_folder
-  template_name = "c8-web-tmpl-a1"
-  storage_profile = "c01-cl02-Tier-2"
-  memory        = 4096
-  cpus          = 2
-  cpu_cores     = 1
-  power_on      = true
-
-  metadata = {
-    role    = "git"
-    env     = "stage"
-    version = "1.0"
-    os  = "linux"
-  }
-
-  network {
-    type               = "org"
-    name               = vcd_vapp_org_network.infra-network.org_network_name
-    ip_allocation_mode = "POOL"
-    ip                 = ""
-    is_primary         = true
-  }
-  
-  customization {
-    force                      = false
-    change_sid                 = false
-    allow_local_admin_password = false
-    auto_generate_password     = false
-    # Other customization options to override the ones from template
-  }
-  
- # disk {
- #   name        = vcd_independent_disk.gitDisk1.name
- #   bus_number  = 1
- #   unit_number = 0
- # }
-}
-
-###### vApp_PG ######
-resource "vcd_vapp_vm" "smpay-t-db-1" {
-  vapp_name     = "vApp_PG"
-  name          = "smpay-t-pg-1"
-  computer_name = "smpay-t-pg-1"
-  catalog_name  = var.template_folder
-  template_name = "c8-web-tmpl-a1"
-  storage_profile = "c01-cl02-Tier-1"
-  memory        = 2048
-  cpus          = 1
-  cpu_cores     = 1
-  power_on      = true
-
-  metadata = {
-    role    = "db"
-    env     = "stage"
-    version = "1.0"
-    os  = "linux"
-  }
-
-  network {
-    type               = "org"
-    name               = vcd_vapp_org_network.inside-network.org_network_name
-    ip_allocation_mode = "POOL"
-    ip                 = ""
-    is_primary         = true
-  }
-  
-  customization {
-    force                      = false
-    change_sid                 = false
-    allow_local_admin_password = false
-    auto_generate_password     = false
-    # Other customization options to override the ones from template
-  }
-}
- 
-resource "vcd_vapp_vm" "smpay-t-pg-2" {
-  vapp_name     = "vApp_PG"
-  name          = "smpay-t-pg-2"
-  computer_name = "smpay-t-pg-2"
-  catalog_name  = var.template_folder
-  template_name = "c8-web-tmpl-a1"
-  memory        = 2048
-  cpus          = 1
-  cpu_cores     = 1
-  power_on      = true
-
-  metadata = {
-    role    = "db"
-    env     = "stage"
-    version = "1.0"
-    os  = "linux"
-  }
-
-  network {
-    type               = "org"
-    name               = vcd_vapp_org_network.inside-network.org_network_name
-    ip_allocation_mode = "POOL"
-    ip                 = ""
-    is_primary         = true
-  }
-  
-  customization {
-    force                      = false
-    change_sid                 = false
-    allow_local_admin_password = false
-    auto_generate_password     = false
-    # Other customization options to override the ones from template
-  }
-}
-
-resource "vcd_vapp_vm" "smpay-t-pg-3" {
-  vapp_name     = "vApp_PG"
-  name          = "smpay-t-pg-3"
-  computer_name = "smpay-t-pg-3"
-  catalog_name  = var.template_folder
-  template_name = "c8-web-tmpl-a1"
-  storage_profile = "c01-cl02-Tier-1"
-  memory        = 2048
-  cpus          = 1
-  cpu_cores     = 1
-  power_on      = true
-
-  metadata = {
-    role    = "db"
-    env     = "stage"
-    version = "1.0"
-    os  = "linux"
-  }
-
-  network {
-    type               = "org"
-    name               = vcd_vapp_org_network.inside-network.org_network_name
-    ip_allocation_mode = "POOL"
-    ip                 = ""
-    is_primary         = true
-  }
-  
-  customization {
-    force                      = false
-    change_sid                 = false
-    allow_local_admin_password = false
-    auto_generate_password     = false
-    # Other customization options to override the ones from template
-  }
-}
-
 ###### vApp_L7LB ######
 resource "vcd_vapp_vm" "smpay-t-l7lb-1" {
   vapp_name     = "vApp_L7LB"
@@ -385,6 +234,7 @@ resource "vcd_vapp_vm" "smpay-t-app-2" {
   }
 }
 
+######## vApp_L7Lb #########
 resource "vcd_vapp_vm" "smpay-t-k8s-master1" {
   vapp_name     = "vApp_k8s"
   name          = "smpay-t-k8s-master1"
@@ -581,6 +431,86 @@ resource "vcd_vapp_vm" "smpay-t-k8s-node3" {
   network {
     type               = "org"
     name               = vcd_vapp_org_network.dmz-network.org_network_name
+    ip_allocation_mode = "POOL"
+    ip                 = ""
+    is_primary         = true
+  }
+  
+  customization {
+    force                      = false
+    change_sid                 = false
+    allow_local_admin_password = false
+    auto_generate_password     = false
+    # Other customization options to override the ones from template
+  }
+}
+
+###### vApp_Infra ######
+resource "vcd_vapp_vm" "smpay-t-gitlab-1" {
+  vapp_name     = "vApp_Infra"
+  name          = "smpay-t-gitlab-1"
+  computer_name = "smpay-t-gitlab-1"
+  catalog_name  = var.template_folder
+  template_name = "c8-web-tmpl-a1"
+  storage_profile = "c01-cl02-Tier-2"
+  memory        = 4096
+  cpus          = 2
+  cpu_cores     = 1
+  power_on      = true
+
+  metadata = {
+    role    = "git"
+    env     = "stage"
+    version = "1.0"
+    os  = "linux"
+  }
+
+  network {
+    type               = "org"
+    name               = vcd_vapp_org_network.infra-network.org_network_name
+    ip_allocation_mode = "POOL"
+    ip                 = ""
+    is_primary         = true
+  }
+  
+  customization {
+    force                      = false
+    change_sid                 = false
+    allow_local_admin_password = false
+    auto_generate_password     = false
+    # Other customization options to override the ones from template
+  }
+  
+ # disk {
+ #   name        = vcd_independent_disk.gitDisk1.name
+ #   bus_number  = 1
+ #   unit_number = 0
+ # }
+}
+
+###### vApp_PG ######
+resource "vcd_vapp_vm" "smpay-t-pg-1" {
+  vapp_name     = "vApp_PG"
+  name          = "smpay-t-pg-1"
+  computer_name = "smpay-t-pg-1"
+  catalog_name  = var.template_folder
+  template_name = "c8-web-tmpl-a1"
+  storage_profile = "c01-cl02-Tier-1"
+  memory        = 2048
+  cpus          = 1
+  cpu_cores     = 1
+  power_on      = true
+
+  metadata = {
+    role    = "db"
+    env     = "stage"
+    version = "1.0"
+    os  = "linux"
+  }
+
+  network {
+    type               = "org"
+    name               = vcd_vapp_org_network.inside-network.org_network_name
     ip_allocation_mode = "POOL"
     ip                 = ""
     is_primary         = true
